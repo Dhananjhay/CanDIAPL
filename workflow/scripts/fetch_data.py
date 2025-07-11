d78662ac0a83142380cd730b182251b2d012fc73
Dhananjhay <dhananjhay03@gmail.com>
2025-07-10 21:31:26 -0400

    add a config file

--- workflow/scripts/fetch_data.py
+++ workflow/scripts/fetch_data.py
@@ -4,18 +4,23 @@ import pyvo
 # TAP service endpoint
 service = pyvo.dal.TAPService("https://gea.esac.esa.int/tap-server/tap")
 
-query = """
+# Load params
+output_file = snakemake.output["out_path"]
+ra = snakemake.params["ra"]
+dec = snakemake.params["dec"]
+radius = snakemake.params["radius"]
+
+query = f"""
 SELECT TOP 100
   source_id, ra, dec, parallax, phot_g_mean_mag,
-  DISTANCE(36.033303, 18.25795, ra, dec) AS ang_sep
+  DISTANCE({ra}, {dec}, ra, dec) AS ang_sep
 FROM gaiadr3.gaia_source
-WHERE DISTANCE(36.033303, 18.25795, ra, dec) < 0.1
+WHERE DISTANCE({ra}, {dec}, ra, dec) < {radius}
 ORDER BY ang_sep ASC
 """
 
 results = service.search(query)
 table = results.to_table()
-output_file = snakemake.output["out_path"]
 
 # Ensure output directory exists
 os.makedirs(os.path.dirname(output_file), exist_ok=True)
